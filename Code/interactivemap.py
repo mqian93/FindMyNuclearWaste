031c2b8af824683546f8302e11d9f61631100b56
mqian93 <mq93@cornell.edu>
2025-10-30 13:24:39 -0400

    Changed map color

--- Code/interactivemap.py
+++ Code/interactivemap.py
@@ -2,8 +2,8 @@ import pandas as pd
 import folium
 from folium import CircleMarker
 
-# 1. Load the dataset
-df = pd.read_csv("CSVs/Cleaned_US_Nuclear_Fuel_Locations.csv")
+# 1. Load the dataset, change to ur own filepath
+df = pd.read_csv("/Users/melodyqian/Documents/GitHub/FindMyNuclearWaste/CSVs/Cleaned_US_Nuclear_Fuel_Locations.csv")
 
 # 2. Filter out rows missing coordinates or Site_Assy
 map_data = df.dropna(subset=["Latitude", "Longitude", "Site_Assy"])
@@ -15,8 +15,8 @@ m = folium.Map(location=[39.5, -98.35], zoom_start=5, tiles="CartoDB positron")
 for _, row in map_data.iterrows():
     CircleMarker(
         location=[row["Latitude"], row["Longitude"]],
-        radius=max(3, row["Site_Assy"] / 1000),  # Scale size by assemblies
-        color="blue",
+        radius=max(3, row["Site_Assy"] / 500),  # Scale size by assemblies
+        color="lightgreen",
         fill=True,
         fill_opacity=0.6,
         popup=folium.Popup(
@@ -27,5 +27,5 @@ for _, row in map_data.iterrows():
     ).add_to(m)
 
 # 5. Save and show map
-m.save("nuclear_sites_map.html")
+m.save("nuclear_sites_map_recolor.html")
 m
