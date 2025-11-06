029b03b1fcdc14e3bb74f295c3dd21a047633498
mqian93 <mq93@cornell.edu>
2025-11-06 00:40:32 -0500

    Finished visuals

---
+++ Code/piechart.py
@@ -0,0 +1,59 @@
+#import modules
+import plotly.graph_objects as go
+import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from plotly.offline import plot
+
+#Import data
+state_filepath='/Users/melodyqian/Documents/GitHub/FindMyNuclearWaste/CSVs/State_Level_Demographics_filtered.csv'
+statedf=pd.read_csv(state_filepath)
+site_filepath='/Users/melodyqian/Documents/GitHub/FindMyNuclearWaste/CSVs/MasterDataset.csv'
+sitedf=pd.read_csv(site_filepath)
+
+#Clean
+sitedf= sitedf[sitedf['county_data']!=1]
+sitedf=sitedf[sitedf['Type']!= 'Government Facility/Multiple']
+
+#Prep data
+sitelabelstor = sitedf['Storage'].value_counts().index
+sitevaluestor = sitedf['Storage'].value_counts().values
+sitelabelurb= sitedf['Urban_Rural'].value_counts().index
+sitevalueurb= sitedf['Urban_Rural'].value_counts().values
+
+###Graphs
+
+#Storage
+colors1 = ['DarkKhaki', 'CadetBlue','Khaki']
+storfig=go.Figure(data=go.Pie(
+     values=sitevaluestor,
+     labels=sitelabelstor,
+     #domain=dict(x=[0, 0.5]),
+     name="Storage Types",
+     textposition='outside',
+     marker=dict(colors=colors1, line=dict(color="#FFFFFF", width=2)),
+     hole=.3))
+
+storfig.update_layout(
+    title_text = 'Type of Waste Storage',
+    width=500
+)
+
+#Urban/Rural
+colors2 = ['PaleGoldenRod', 'DarkSeaGreen','White', 'DarkGrey']
+urbfig= go.Figure(data=go.Pie(
+     values=sitevalueurb,
+     labels=sitelabelurb,
+     #domain=dict(x=[0.5, 1.0]),
+     name="Urban/Rural",
+     textposition='outside',
+     marker=dict(colors=colors2, line=dict(color="#FFFFFF", width=2)),
+     hole=.3))
+
+urbfig.update_layout(
+    title_text = 'Urban-Rural Classification of Waste Disposal Sites',
+    width=500
+)
+
+storfig.write_html('storagepie.html')
+urbfig.write_html('urbanpie.html')
---
+++ Code/racetn_bar.py
@@ -0,0 +1,80 @@
+#Import modules
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import numpy
+
+#read data
+state_filepath='/Users/melodyqian/Documents/GitHub/FindMyNuclearWaste/CSVs/State_Level_Demographics_filtered.csv'
+statedf=pd.read_csv(state_filepath)
+site_filepath='/Users/melodyqian/Documents/GitHub/FindMyNuclearWaste/CSVs/MasterDataset.csv'
+sitedf=pd.read_csv(site_filepath)
+
+#Clean
+sitedf= sitedf[sitedf['county_data']!=1]
+sitedf=sitedf[sitedf['Type']!= 'Government Facility/Multiple']
+
+#Rearrange data
+statecolumns=['white_percent', 'black_percent', 'asian_percent', 'pacific_percent', 'native_percent', 'hispanic_percent', 'nonhispanic_percent']
+sitecolumns=['white_percent', 'black_percent', 'asian_percent', 'pacific_percent', 'native_percent', 'hispanic_percent', 'nonhispanic_percent']
+
+meltedstate = statedf[statecolumns].melt(var_name='Demographic', value_name='Value')
+mstate = meltedstate.groupby('Demographic')['Value'].mean().reset_index()
+meltedsite = sitedf[sitecolumns].melt(var_name='Demographic', value_name='Value')
+msite = meltedsite.groupby('Demographic')['Value'].mean().reset_index()
+
+# Rename reorder
+rename_map = {
+    'white_percent': 'White',
+    'black_percent': 'Black',
+    'asian_percent': 'Asian',
+    'pacific_percent': 'Pacific Islander',
+    'native_percent': 'Native',
+    'hispanic_percent': 'Hispanic',
+    'nonhispanic_percent': 'Non-Hispanic'
+}
+mstate['Demographic'] = mstate['Demographic'].replace(rename_map)
+msite['Demographic'] = msite['Demographic'].replace(rename_map)
+order = ['White', 'Black', 'Asian', 'Pacific Islander', 'Native', 'Hispanic', 'Non-Hispanic']
+
+# Sort
+mstate['Demographic'] = pd.Categorical(mstate['Demographic'], categories=order, ordered=True)
+mstate = mstate.sort_values('Demographic').reset_index(drop=True)
+
+msite['Demographic'] = pd.Categorical(msite['Demographic'], categories=order, ordered=True)
+msite = msite.sort_values('Demographic').reset_index(drop=True)
+
+#Hover text
+msite['hover_text'] = msite.apply(
+    lambda row: f"{row['Demographic']}: {round(row['Value'] * 100, 2)}%", axis=1
+)
+mstate['hover_text'] = mstate.apply(
+    lambda row: f"{row['Demographic']}: {round(row['Value'] * 100, 2)}%", axis=1
+)
+
+#Graph
+racetnfig= go.Figure()
+racetnfig.add_trace(go.Bar(
+    x=mstate['Demographic'],
+    y=mstate['Value'],
+    name='State',
+    marker_color='rgb(129, 127, 0)',
+    text=mstate['hover_text'],
+    textposition='none'
+))
+racetnfig.add_trace(go.Bar(
+    x=msite['Demographic'],
+    y=msite['Value'],
+    name='Site',
+    text=msite['hover_text'],
+    marker_color='rgb(211, 228, 36)',
+    textposition='none'
+))
+ 
+racetnfig.update_layout(
+    title_text = 'Racial and Ethnic Makeup Comparison',
+    width=900
+)
+
+racetnfig.write_html('racetnbar.html')
+
