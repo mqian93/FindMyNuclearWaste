dbf5ccf3b94f5af7a5244016d983994dc5da7f6f
mqian93 <mq93@cornell.edu>
2025-11-05 19:05:34 -0500

    Making Visuals

--- Code/mergedata.py
+++ Code/mergedata.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 #import datasets
-fueldf= pd.read_csv("/Users/melodyqian/Documents/GitHub/FindMyNuclearWaste/CSVs/Cleaned_US_Nuclear_Fuel_Locations.csv")
+fueldf= pd.read_csv("/Users/melodyqian/Documents/GitHub/FindMyNuclearWaste/CSVs/Cleaned_US_Nuclear_fuel_rural.csv")
 demodf=pd.read_csv("/Users/melodyqian/Documents/GitHub/FindMyNuclearWaste/CSVs/DemographicEconomicInfo.csv")
 
 #merge based on 1st few digits of latitude and longitude
+fueldf['Latitude']= fueldf['Latitude'].round(2)
+demodf['Latitude']= demodf['latitude'].round(2)
+
+mergedf=pd.merge(fueldf, demodf, on='Latitude')
+mergedf.to_csv("MasterDataset.csv")
